7448a95ad3c0a61d6a282a3ec2510f9fd2bc37ba
Leibril <ariabi.sr@gmail.com>
2024-07-29 08:05:32 -0600

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Doctores
+        * Enfermeras
+        * Secretaria
+        * Pacientes
+        * Círujano
+        * Auxiliares de enfermería
+        * Fisioterapeutas
+        * Logopedas
+        * Farmaceeúticos hospitaliaros
+        * Enfermeras en práctica
+        * Conductor de Ambulancia
+        * Paramédicos
+        * Personal de Seguridad
+        *
+        * */
+
+        // SALAS O HABITACIONES
+        /* Cirujia
+        * Sala de emergencias
+        * Seguridad
+        * Radiografía
+        *
+        * */
+        //
+
+
+    }
+}
