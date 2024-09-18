96686618b01c74c16e311c2c388d4817f82c1152
Leibril <ariabi.sr@gmail.com>
2024-09-17 18:46:33 -0600

    DoctorLayout
    Se crea el encabezado para la ventana del doctor, el cual
    contiene el nombre del hospital, el Logo, el nombre del doctor y
    su especialidad.
    
    La clase DoctorView contiene:
    - El método constructor convertido en una ventana.
    - Como administrador de diseño se utilizó un BorderLayout y GridBagLayout
    (se utilizó en el panel "userPanel").

---
+++ src/hospital/view/DoctorView.java
@@ -0,0 +1,88 @@
+package hospital.view;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class DoctorView extends JFrame {
+
+    private int[] pantalla = {1300,800};
+
+
+    public DoctorView(){
+
+        //----------- -----------VENTANA--------------------
+        this.setSize(pantalla[0], pantalla[1]);
+        this.setLayout(new BorderLayout());
+        this.setTitle("Perfil del doctor");
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        //----------- -----------HEADER--------------------
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        headerPanel.setPreferredSize(new Dimension(1300, 60));
+        headerPanel.setBackground(Color.darkGray);
+
+        // NOMBRE DEL HOSPITAL
+        JLabel nombreHospital = new JLabel("Hospital Santa Catalina");
+        nombreHospital.setForeground(Color.white);
+        nombreHospital.setFont(new Font("Liberation Sans", Font.BOLD, 16));
+        nombreHospital.setBorder(new EmptyBorder(0, 30, 0, 0)); // establece un margen
+
+        //------------------LOGO Y USUARIO DEL DOCTOR-------------------
+        JPanel userPanel = new JPanel(new GridBagLayout());
+        userPanel.setPreferredSize(new Dimension(270,60));
+        userPanel.setBackground(Color.darkGray);
+
+        // LOGO
+        JPanel representacionLogo = new JPanel();
+        representacionLogo.setPreferredSize(new Dimension(40,40));
+        representacionLogo.setBackground(Color.orange);
+
+        GridBagConstraints restricciones = new GridBagConstraints();
+        restricciones.gridx = 0;
+        restricciones.gridy = 0;
+        restricciones.gridheight = 2;
+        restricciones.insets = new Insets(0,14,0,0);
+
+        userPanel.add(representacionLogo, restricciones);
+
+        // NOMBRE DOCTOR
+
+        JLabel nombreDoc = new JLabel("Aaron Almendro");
+        nombreDoc.setFont(new Font("Liberation Sans", Font.BOLD, 15));
+        nombreDoc.setForeground(Color.white);
+
+        restricciones.gridx = 1;
+        restricciones.gridy = 0;
+        restricciones.gridheight = 1;
+
+        userPanel.add(nombreDoc, restricciones);
+
+        // ESPECIALIDAD
+
+        JLabel especialidadDoc = new JLabel("Doctor General");
+        especialidadDoc.setFont(new Font("Liberation Sans", Font.PLAIN, 13));
+        especialidadDoc.setForeground(Color.white);
+
+        restricciones.gridx = 1;
+        restricciones.gridy = 1;
+        restricciones.gridheight = 2;
+        restricciones.anchor = GridBagConstraints.WEST;
+
+        userPanel.add(especialidadDoc, restricciones);
+
+        // AGREGACIONES
+        headerPanel.add(userPanel, BorderLayout.EAST);
+        headerPanel.add(nombreHospital, BorderLayout.WEST);
+        this.add(headerPanel, BorderLayout.NORTH);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        DoctorView doc = new DoctorView();
+    }
+
+
+}
